988842977de6c4755660fe0b4af11ff8fa12482a
Kamil Korbik <korbik.gm@gmail.com>
2021-05-05 00:29:54 +0200

    Added Job Model

---
+++ src/main/java/com/example/job/model/Job.java
@@ -0,0 +1,33 @@
+package com.example.job.model;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "JOB")
+public class Job {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "Id")
+    private Long id;
+    @Column(name = "Name")
+    private String name;
+
+    public Job(){}
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
