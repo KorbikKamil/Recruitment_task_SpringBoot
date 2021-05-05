1a49884f19866b4b8c46c5ead98dd054cadf5bcf
Kamil Korbik <korbik.gm@gmail.com>
2021-05-05 02:51:35 +0200

    Added simple intergration tests

--- src/test/java/com/example/job/controllers/JobControllerTest.java
+++ src/test/java/com/example/job/controllers/JobControllerTest.java
@@ -3,16 +3,19 @@ package com.example.job.controllers;
 import com.example.job.controller.JobController;
 import com.example.job.model.Job;
 import com.example.job.repository.JobRepository;
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.json.JSONArray;
 import org.json.JSONObject;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.objectweb.asm.TypeReference;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
@@ -24,6 +27,7 @@ import java.io.DataInput;
 import java.util.Arrays;
 import java.util.List;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
@@ -47,7 +51,6 @@ public class JobControllerTest {
     @Test
     void shouldGetJobs() throws Exception {
         // given
-
         //when
         MvcResult mvcResult = mockMvc.perform(get("/jobs"))
                 .andDo(print())
@@ -57,7 +60,23 @@ public class JobControllerTest {
         JSONObject jsonObj = new JSONObject(mvcResult.getResponse().getContentAsString());
         JSONArray jsonArray = jsonObj.getJSONArray("content");
         List<Job> jobsList = Arrays.asList(objectMapper.readValue(jsonArray.toString(), Job[].class));
-        for(Job j : jobsList)
-            System.out.println(j);
+        assertEquals(3, jobsList.size());
+        assertEquals("job1", jobsList.get(0).getName());
+    }
+
+    @Test
+    void shouldAddPost() throws Exception {
+        // given
+        Job job = new Job("job4");
+        String inputJson = objectMapper.writeValueAsString(job);
+        //when
+        MvcResult mvcResult = mockMvc.perform(MockMvcRequestBuilders.post("/jobs")
+                .contentType(MediaType.APPLICATION_JSON)
+        .content(inputJson)).andReturn();
+        //then
+        assertEquals(201, mvcResult.getResponse().getStatus());
+        Job returnedJob = objectMapper.readValue(mvcResult.getResponse().getContentAsString(), Job.class);
+        assertEquals(4, returnedJob.getId());
+        assertEquals("job4", returnedJob.getName());
     }
 }
